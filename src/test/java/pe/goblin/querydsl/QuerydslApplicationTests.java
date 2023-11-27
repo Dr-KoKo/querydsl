fe3d301a04a09ca8c23df98bc152f8d65ded715b
Denny-Ko <donghar@naver.com>
2023-11-27 23:47:12 +0900

    Querydsl 설정과 검증

--- src/test/java/pe/goblin/querydsl/QuerydslApplicationTests.java
+++ src/test/java/pe/goblin/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,37 @@
 package pe.goblin.querydsl;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import pe.goblin.querydsl.entity.Hello;
+import pe.goblin.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
-	}
+		Hello hello = new Hello();
+		em.persist(hello);
 
+		JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+		// QHello qHello = QHello.hello;
+
+		Hello result = jpaQueryFactory
+			.selectFrom(qHello)
+			.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+	}
 }
