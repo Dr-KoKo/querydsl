ca67b2b33e21e098bee8bab6affec820dfcde77c
Denny-Ko <donghar@naver.com>
2023-12-03 16:25:30 +0900

    서브 쿼리

--- src/test/java/pe/goblin/querydsl/QuerydslBasicTest.java
+++ src/test/java/pe/goblin/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package pe.goblin.querydsl;
 
+import static com.querydsl.jpa.JPAExpressions.*;
 import static pe.goblin.querydsl.entity.QMember.*;
 import static pe.goblin.querydsl.entity.QTeam.*;
 
@@ -336,5 +337,82 @@ public class QuerydslBasicTest {
 
 		boolean isLoaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
 		Assertions.assertThat(isLoaded).as("패치 조인 미적용").isTrue();
+	}
+
+	/**
+	 * 나이가 가장 많은 회원 조회
+	 */
+	@Test
+	public void subQuery() {
+		QMember memberSub = new QMember("memberSub");
+
+		List<Member> result = queryFactory
+			.selectFrom(member)
+			.where(member.age.eq(
+				select(memberSub.age.max())
+					.from(memberSub)
+			))
+			.fetch();
+
+		Assertions.assertThat(result)
+			.extracting("age")
+			.containsExactly(40);
+	}
+
+	/**
+	 * 나이가 평균 이상인 회원
+	 */
+	@Test
+	public void subQueryGoe() {
+		QMember memberSub = new QMember("memberSub");
+
+		List<Member> result = queryFactory
+			.selectFrom(member)
+			.where(member.age.goe(
+				select(memberSub.age.avg())
+					.from(memberSub)
+			))
+			.fetch();
+
+		Assertions.assertThat(result)
+			.extracting("age")
+			.containsExactly(30, 40);
+	}
+
+	/**
+	 * 나이가 10살 이상인 회원
+	 */
+	@Test
+	public void subQueryIn() {
+		QMember memberSub = new QMember("memberSub");
+
+		List<Member> result = queryFactory
+			.selectFrom(member)
+			.where(member.age.in(
+				select(memberSub.age)
+					.from(memberSub)
+					.where(memberSub.age.goe(10))
+			))
+			.fetch();
+
+		Assertions.assertThat(result)
+			.extracting("age")
+			.containsExactly(20, 30, 40);
+	}
+
+	@Test
+	public void selectSubQuery() {
+		QMember memberSub = new QMember("memberSub");
+
+		List<Tuple> result = queryFactory
+			.select(member.username,
+				select(memberSub.age.avg())
+					.from(memberSub))
+			.from(member)
+			.fetch();
+
+		for (Tuple tuple : result) {
+			System.out.println("tuple = " + tuple);
+		}
 	}
 }
