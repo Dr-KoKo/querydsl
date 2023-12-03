52e984897c70f1308bcf2b072af14ec8908f612c
Denny-Ko <donghar@naver.com>
2023-12-03 18:22:45 +0900

    상수,문자 더하기

--- src/test/java/pe/goblin/querydsl/QuerydslBasicTest.java
+++ src/test/java/pe/goblin/querydsl/QuerydslBasicTest.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import jakarta.persistence.EntityManager;
@@ -444,6 +445,32 @@ public class QuerydslBasicTest {
 
 		for (String age : result) {
 			System.out.println("age = " + age);
+		}
+	}
+
+	@Test
+	public void constant() {
+		List<Tuple> result = queryFactory
+			.select(member.username, Expressions.constant("A"))
+			.from(member)
+			.fetch();
+
+		for (Tuple tuple : result) {
+			System.out.println("tuple = " + tuple);
+		}
+	}
+
+	@Test
+	public void concat() {
+		// {username}_{age}
+		List<String> result = queryFactory
+			.select(member.username.concat("_").concat(member.age.stringValue()))
+			.from(member)
+			.where(member.username.eq("member1"))
+			.fetch();
+
+		for (String s : result) {
+			System.out.println("s = " + s);
 		}
 	}
 }
