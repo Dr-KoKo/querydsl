3d2391b900d47d96b31bfc7464cda2be85039108
Denny-Ko <donghar@naver.com>
2023-12-04 23:34:37 +0900

    스프링 데이터 페이징 활용1 - Querydsl 페이징 연동

--- src/main/java/pe/goblin/querydsl/repository/MemberCustomRepository.java
+++ src/main/java/pe/goblin/querydsl/repository/MemberCustomRepository.java
@@ -2,9 +2,16 @@ package pe.goblin.querydsl.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import pe.goblin.querydsl.dto.MemberSearchCondition;
 import pe.goblin.querydsl.dto.MemberTeamDto;
 
 public interface MemberCustomRepository {
 	List<MemberTeamDto> search(MemberSearchCondition condition);
+
+	Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+
+	Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
