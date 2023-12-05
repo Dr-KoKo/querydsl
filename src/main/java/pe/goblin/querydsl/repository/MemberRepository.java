f2ba32b5221edab2c34ea1200906c8df12bf60c8
Denny-Ko <donghar@naver.com>
2023-12-05 11:11:49 +0900

    인터페이스 지원 - QuerydslPredicateExecutor

--- src/main/java/pe/goblin/querydsl/repository/MemberRepository.java
+++ src/main/java/pe/goblin/querydsl/repository/MemberRepository.java
@@ -3,9 +3,11 @@ package pe.goblin.querydsl.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import pe.goblin.querydsl.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
+public interface MemberRepository
+	extends JpaRepository<Member, Long>, MemberCustomRepository, QuerydslPredicateExecutor<Member> {
 	List<Member> findByUsername(String username);
 }
